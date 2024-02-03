74c6b95d1215178ae28aa351b60e37f09fb68d47
Nastya <nastshegoleva@yandex.ru>
2024-02-03 19:33:52 +0300

    Добавление обработки клавиш вверх/вниз/вправо/влево

--- main.py
+++ main.py
@@ -4,14 +4,32 @@ import sys
 import pygame
 import requests
 
+coords = (55.530887, 55.703118)
 
-def draw_map(size):
-    global map_file
+
+def draw_map(size, dir=(0, 0)):
+    global map_file, coords
+    current_lattitude, current_longitude = coords
+
     zoom = size
-    coords = "55.530887,55.703118"
+    x_move, y_move = dir
+    rect = 2 ** (17 - z) * 0.002
+    current_lattitude += (x_move * rect / 2) / (1 + (current_lattitude) / 90)
+    current_longitude += y_move * rect / 2
+    if current_longitude > 85:
+        current_longitude = 85
+    if current_longitude < -85:
+        current_longitude = -85
+    if current_lattitude > 180:
+        current_lattitude -= 360
+    if current_lattitude < -180:
+        current_lattitude += 360
+    coords = f'{current_lattitude},{current_longitude}'
     map_request = "https://static-maps.yandex.ru/1.x/?ll=" + coords + "&"
     map_request += "z=" + str(zoom) + "&l=sat"
     response = requests.get(map_request)
+    coords = (current_lattitude, current_longitude)
+
     if not response:
         print("Ошибка выполнения запроса:")
         print(map_request)
@@ -47,6 +65,14 @@ while running:
                 if z < 1:
                     z = 1
                 draw_map(z)
+            if event.key == pygame.K_UP:
+                draw_map(z, (0, 1))
+            if event.key == pygame.K_DOWN:
+                draw_map(z, (0, -1))
+            if event.key == pygame.K_LEFT:
+                draw_map(z, (-1, 0))
+            if event.key == pygame.K_RIGHT:
+                draw_map(z, (1, 0))
 pygame.quit()
 
 # Удаляем за собой файл с изображением.
